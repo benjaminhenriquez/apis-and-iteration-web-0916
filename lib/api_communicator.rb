c1ba4636ed85be11293a3f4e28422d646243c0aa
Ben <henriquez.ben@gmail.com>
2016-09-28 16:00:52 -0400

    day 3

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,27 +1,39 @@
 require 'rest-client'
+require 'JSON'
+require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
+
+def get_characters_from_api(character)
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  # iterate ove the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film. 
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
-  #  of movies by title. play around with puts out other info about a given film.
+  return character_hash
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def character_films(character)
+  character_hash = get_characters_from_api(character)
+  if current_character = character_hash["results"].find {|character_data| character_data["name"].downcase == character}
+    current_films = current_character.fetch("films")
+    return current_films
+  else
+    return nil
+  end
+end
+
+def parse_character_movies(film_arr)
+  if (film_arr == nil)
+    puts "Character Doesn't Exist"
+  else
+    film_arr.map do |movie_url|
+      all_movies = RestClient.get(movie_url)
+      movie_hash = JSON.parse(all_movies)
+      puts movie_hash["title"]
+    end
+  end
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  films_arr = character_films(character)
+  parse_character_movies(films_arr)
 end
 
 ## BONUS
